30832d2326246646bc9429f2b2100569fbd53eee
midskavid <midskavid@yahoo.co.in>
2020-02-15 21:54:20 +0000

    Adding shared memory access for tiles

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -7,19 +7,29 @@
 using namespace std;
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
-    int i = threadIdx.y;
-    int j = threadIdx.x;
+    const int TK = 32;
+    __shared__ _DOUBLE_ As[TK][TK], Bs[TK][TK];
 
-    int ii = blockIdx.y;
-    int jj = blockIdx.x;
+    int ty = threadIdx.y;
+    int tx = threadIdx.x;
 
-    int TK = blockDim.x;
+    int by = blockIdx.y;
+    int bx = blockIdx.x;
+
+    int I = by*TK+ty;
+    int J = bx*TK+tx;
     
     _DOUBLE_ Cij = 0;
-    for (int kk=0;kk<N;kk+=TK) {
-        for (int k = kk;k<min(kk+TK,N);++k) {
-            Cij += A[(ii*TK+i)*N + k] * B[k*N+jj*TK+j]; 
+
+    int nTK = N/TK;
+    for (int kk=0;kk<nTK;++kk) {
+        As[ty][tx] = A[I*N + kk*TK+tx];
+        Bs[ty][tx] = B[(kk*TK+ty)*N + J];
+        __syncthreads();
+        for (int k=0;k<TK;++k) {
+            Cij += As[ty][k] * Bs[k][tx]; 
         }
+        __syncthreads();
     }
-    C[(ii*TK+i)*N + jj*TK+j] = Cij;
+    C[I*N+J] = Cij;
 }
