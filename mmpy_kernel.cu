8031970586561ec0cdba23fb82daefa79db7c461
midskavid <midskavid@yahoo.co.in>
2020-02-17 09:13:46 +0000

    Handling if checks

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -78,43 +78,68 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 }
 #else
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
-    __shared__ _DOUBLE_ As0[2*BLOCK_SIZE][BLOCK_SIZE], Bs0[BLOCK_SIZE][2*BLOCK_SIZE];
+    if (!(N&(N-1))) {
+        __shared__ _DOUBLE_ As0[2*BLOCK_SIZE][BLOCK_SIZE], Bs0[BLOCK_SIZE][2*BLOCK_SIZE];
 
-    int ty = threadIdx.y;
-    int tx = threadIdx.x;
-    int by0 = 2*blockIdx.y;
-    int bx0 = 2*blockIdx.x;
+        int ty = threadIdx.y;
+        int tx = threadIdx.x;
+        int by0 = 2*blockIdx.y;
+        int bx0 = 2*blockIdx.x;
 
-    int I0 = by0*BLOCK_SIZE+ty;
-    int I1 = (by0+1)*BLOCK_SIZE+ty;
+        int I0 = by0*BLOCK_SIZE+ty;
+        int I1 = (by0+1)*BLOCK_SIZE+ty;
 
-    int J0 = bx0*BLOCK_SIZE+tx;
-    int J1 = (bx0+1)*BLOCK_SIZE+tx;
+        int J0 = bx0*BLOCK_SIZE+tx;
+        int J1 = (bx0+1)*BLOCK_SIZE+tx;
 
-    _DOUBLE_ Cij[4] = {0,0,0,0};
+        _DOUBLE_ Cij[4] = {0,0,0,0};
 
-    int nBLOCK_SIZE = N/BLOCK_SIZE;
-#pragma unroll 4
-    for (unsigned int kk=0;kk<nBLOCK_SIZE;++kk) { // should be less than or equal to, right?
-        As0[ty][tx] = A[I0*N + kk*BLOCK_SIZE+tx];
-        As0[ty+32][tx] = A[I1*N + kk*BLOCK_SIZE+tx];
+        int nBLOCK_SIZE = N/BLOCK_SIZE;
+    #pragma unroll 4
+        for (unsigned int kk=0;kk<nBLOCK_SIZE;++kk) { // should be less than or equal to, right?
+            As0[ty][tx] = A[I0*N + kk*BLOCK_SIZE+tx];
+            As0[ty+32][tx] = A[I1*N + kk*BLOCK_SIZE+tx];
 
-        Bs0[ty][tx] = B[(kk*BLOCK_SIZE+ty)*N + J0];
-        Bs0[ty][tx+32] = B[(kk*BLOCK_SIZE+ty)*N + J1];
+            Bs0[ty][tx] = B[(kk*BLOCK_SIZE+ty)*N + J0];
+            Bs0[ty][tx+32] = B[(kk*BLOCK_SIZE+ty)*N + J1];
 
-        __syncthreads();
-#pragma unroll 32  
-        for (unsigned int k=0;k<BLOCK_SIZE;++k) {
-            Cij[0] += As0[ty][k] * Bs0[k][tx];
-            Cij[1] += As0[ty][k] * Bs0[k][tx+32];
-            Cij[2] += As0[ty+32][k] * Bs0[k][tx];
-            Cij[3] += As0[ty+32][k] * Bs0[k][tx+32];
+            __syncthreads();
+    #pragma unroll 32  
+            for (unsigned int k=0;k<BLOCK_SIZE;++k) {
+                Cij[0] += As0[ty][k] * Bs0[k][tx];
+                Cij[1] += As0[ty][k] * Bs0[k][tx+32];
+                Cij[2] += As0[ty+32][k] * Bs0[k][tx];
+                Cij[3] += As0[ty+32][k] * Bs0[k][tx+32];
+            }
+            __syncthreads();
         }
-        __syncthreads();
+        C[I0*N+J0] = Cij[0];
+        C[I0*N+J1] = Cij[1];
+        C[I1*N+J0] = Cij[2];
+        C[I1*N+J1] = Cij[3];
     }
-    C[I0*N+J0] = Cij[0];
-    C[I0*N+J1] = Cij[1];
-    C[I1*N+J0] = Cij[2];
-    C[I1*N+J1] = Cij[3];
+    else {
+        // go naive here..
+        int I0 =  (2*blockIdx.y)*BLOCK_SIZE + threadIdx.y;
+        int I1 =  (2*blockIdx.y+1)*BLOCK_SIZE + threadIdx.y;
+
+        int J0 =  (2*blockIdx.x)*BLOCK_SIZE + threadIdx.x;
+        int J1 =  (2*blockIdx.x+1)*BLOCK_SIZE + threadIdx.x;
+
+        if((I0 < N) && (J0 < N)){
+            _DOUBLE_ _c = 0;
+            for (int k = 0; k < N; k++) {
+                _c += A[I0 * N + k]*B[k * N + J0];
+            }
+            C[I0 * N + J0] = _c;
+        }    
+        if((I1 < N) && (J1 < N)){
+            _DOUBLE_ _c = 0;
+            for (int k = 0; k < N; k++) {
+                _c += A[I1 * N + k]*B[k * N + J1];
+            }
+            C[I1 * N + J1] = _c;
+        }        
+    }
 }
 #endif
