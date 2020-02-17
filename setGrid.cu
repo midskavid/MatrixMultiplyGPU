cdcceb6fffdb47714136af4de4221283ab6ed602
midskavid <midskavid@yahoo.co.in>
2020-02-17 19:52:52 +0000

    Pending changes

--- setGrid.cu
+++ setGrid.cu
@@ -1,8 +1,6 @@
 void setGrid(int n, dim3 &blockDim, dim3 &gridDim)
 {
    // set your block dimensions and grid dimensions here
-   // gridDim.x = ceil(((double)n / blockDim.x)/2);
-   // gridDim.y = ceil(((double)n / blockDim.y)/2);
    gridDim.x = n / blockDim.x/2;
    gridDim.y = n / blockDim.y/2;
    if(n % blockDim.x != 0)
