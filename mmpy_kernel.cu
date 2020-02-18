8a6a6f89c28478e6d7c115061ba7dcc685990e10
Ubuntu <ubuntu@ip-172-31-14-137.us-west-2.compute.internal>
2020-02-18 01:29:01 +0000

    16 outputs/thread, block_size=16, GFlops getting to 600!

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -7,109 +7,152 @@
 
 using namespace std;
 
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 16
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
-    __shared__ _DOUBLE_ As0[2*BLOCK_SIZE][BLOCK_SIZE], Bs0[BLOCK_SIZE][2*BLOCK_SIZE];
+    __shared__ _DOUBLE_ As0[4*BLOCK_SIZE][BLOCK_SIZE], Bs0[BLOCK_SIZE][4*BLOCK_SIZE];
 
     int ty = threadIdx.y;
     int tx = threadIdx.x;
-    int by0 = 2*blockIdx.y;
-    int bx0 = 2*blockIdx.x;
+    int by0 = 4*blockIdx.y;
+    int bx0 = 4*blockIdx.x;
 
     int I0 = by0*BLOCK_SIZE+ty;
     int I1 = (by0+1)*BLOCK_SIZE+ty;
+    int I2 = (by0+2)*BLOCK_SIZE+ty;
+    int I3 = (by0+3)*BLOCK_SIZE+ty;
 
     int J0 = bx0*BLOCK_SIZE+tx;
     int J1 = (bx0+1)*BLOCK_SIZE+tx;
+    int J2 = (bx0+2)*BLOCK_SIZE+tx;
+    int J3 = (bx0+3)*BLOCK_SIZE+tx;
 
-    _DOUBLE_ Cij[4] = {0,0,0,0};
+    _DOUBLE_ Cij[16] = {0};
 
-    /*if(N == 256) {
-        I0 = blockIdx.y*BLOCK_SIZE+ty;
-        J0 = blockIdx.x*BLOCK_SIZE+tx;
-
+    if (!(N&(N-1))) { // powers of 2
         int nBLOCK_SIZE = N/BLOCK_SIZE;
         #pragma unroll 4
         for (unsigned int kk=0;kk<nBLOCK_SIZE;++kk) {
             As0[ty][tx] = A[I0*N + kk*BLOCK_SIZE+tx];
-            Bs0[ty][tx] = B[(kk*BLOCK_SIZE+ty)*N + J0];
-
-            __syncthreads();
-            #pragma unroll 32
-            for (unsigned int k=0;k<BLOCK_SIZE;++k) {
-                Cij[0] += As0[ty][k] * Bs0[k][tx];
-            }
-            __syncthreads();
-        }
-        C[I0*N+J0] = Cij[0];
-    }
-    else */if (!(N&(N-1))) { // powers of 2
-        int nBLOCK_SIZE = N/BLOCK_SIZE;
-        #pragma unroll 4
-        for (unsigned int kk=0;kk<nBLOCK_SIZE;++kk) {
-            As0[ty][tx] = A[I0*N + kk*BLOCK_SIZE+tx];
-            As0[ty+32][tx] = A[I1*N + kk*BLOCK_SIZE+tx];
+            As0[ty+16][tx] = A[I1*N + kk*BLOCK_SIZE+tx];
+            As0[ty+32][tx] = A[I2*N + kk*BLOCK_SIZE+tx];
+            As0[ty+48][tx] = A[I3*N + kk*BLOCK_SIZE+tx];
 
             Bs0[ty][tx] = B[(kk*BLOCK_SIZE+ty)*N + J0];
-            Bs0[ty][tx+32] = B[(kk*BLOCK_SIZE+ty)*N + J1];
+            Bs0[ty][tx+16] = B[(kk*BLOCK_SIZE+ty)*N + J1];
+            Bs0[ty][tx+32] = B[(kk*BLOCK_SIZE+ty)*N + J2];
+            Bs0[ty][tx+48] = B[(kk*BLOCK_SIZE+ty)*N + J3];
 
             __syncthreads();
-            #pragma unroll 32
+            #pragma unroll 16
             for (unsigned int k=0;k<BLOCK_SIZE;++k) {
                 Cij[0] += As0[ty][k] * Bs0[k][tx];
-                Cij[1] += As0[ty][k] * Bs0[k][tx+32];
-                Cij[2] += As0[ty+32][k] * Bs0[k][tx];
-                Cij[3] += As0[ty+32][k] * Bs0[k][tx+32];
+                Cij[1] += As0[ty][k] * Bs0[k][tx+16];
+                Cij[2] += As0[ty][k] * Bs0[k][tx+32];
+                Cij[3] += As0[ty][k] * Bs0[k][tx+48];
+                Cij[4] += As0[ty+16][k] * Bs0[k][tx];
+                Cij[5] += As0[ty+16][k] * Bs0[k][tx+16];
+                Cij[6] += As0[ty+16][k] * Bs0[k][tx+32];
+                Cij[7] += As0[ty+16][k] * Bs0[k][tx+48];
+                Cij[8] += As0[ty+32][k] * Bs0[k][tx];
+                Cij[9] += As0[ty+32][k] * Bs0[k][tx+16];
+                Cij[10] += As0[ty+32][k] * Bs0[k][tx+32];
+                Cij[11] += As0[ty+32][k] * Bs0[k][tx+48];
+                Cij[12] += As0[ty+48][k] * Bs0[k][tx];
+                Cij[13] += As0[ty+48][k] * Bs0[k][tx+16];
+                Cij[14] += As0[ty+48][k] * Bs0[k][tx+32];
+                Cij[15] += As0[ty+48][k] * Bs0[k][tx+48];
             }
             __syncthreads();
         }
         C[I0*N+J0] = Cij[0];
         C[I0*N+J1] = Cij[1];
-        C[I1*N+J0] = Cij[2];
-        C[I1*N+J1] = Cij[3];
+        C[I0*N+J2] = Cij[2];
+        C[I0*N+J3] = Cij[3];
+        C[I1*N+J0] = Cij[4];
+        C[I1*N+J1] = Cij[5];
+        C[I1*N+J2] = Cij[6];
+        C[I1*N+J3] = Cij[7];
+        C[I2*N+J0] = Cij[8];
+        C[I2*N+J1] = Cij[9];
+        C[I2*N+J2] = Cij[10];
+        C[I2*N+J3] = Cij[11];
+        C[I3*N+J0] = Cij[12];
+        C[I3*N+J1] = Cij[13];
+        C[I3*N+J2] = Cij[14];
+        C[I3*N+J3] = Cij[15];
     }
     else { // non-powers of 2 (with boundary checks)
-        int ty = threadIdx.y;
-        int tx = threadIdx.x;
-        int by0 = 2*blockIdx.y;
-        int bx0 = 2*blockIdx.x;
-
-        int I0 = by0*BLOCK_SIZE+ty;
-        int I1 = (by0+1)*BLOCK_SIZE+ty;
-
-        int J0 = bx0*BLOCK_SIZE+tx;
-        int J1 = (bx0+1)*BLOCK_SIZE+tx;
-
-        _DOUBLE_ Cij[4] = {0,0,0,0};
-
         int nBLOCK_SIZE = (N/BLOCK_SIZE) + 1;
-        
         #pragma unroll 4
         for (unsigned int kk=0;kk<nBLOCK_SIZE;++kk) {
             As0[ty][tx] = (I0<N && (kk*BLOCK_SIZE+tx) < N) ? A[I0*N + kk*BLOCK_SIZE+tx] : 0;
-            As0[ty+32][tx] = (I1<N && (kk*BLOCK_SIZE+tx) < N) ? A[I1*N + kk*BLOCK_SIZE+tx] : 0;
+            As0[ty+16][tx] = (I1<N && (kk*BLOCK_SIZE+tx) < N) ? A[I1*N + kk*BLOCK_SIZE+tx] : 0;
+            As0[ty+32][tx] = (I2<N && (kk*BLOCK_SIZE+tx) < N) ? A[I2*N + kk*BLOCK_SIZE+tx] : 0;
+            As0[ty+48][tx] = (I3<N && (kk*BLOCK_SIZE+tx) < N) ? A[I3*N + kk*BLOCK_SIZE+tx] : 0;
 
             Bs0[ty][tx] = ((kk*BLOCK_SIZE+ty) < N  && J0<N) ? B[(kk*BLOCK_SIZE+ty)*N + J0] : 0;
-            Bs0[ty][tx+32] = ((kk*BLOCK_SIZE+ty) < N  && J1<N) ? B[(kk*BLOCK_SIZE+ty)*N + J1] : 0;
+            Bs0[ty][tx+16] = ((kk*BLOCK_SIZE+ty) < N  && J1<N) ? B[(kk*BLOCK_SIZE+ty)*N + J1] : 0;
+            Bs0[ty][tx+32] = ((kk*BLOCK_SIZE+ty) < N  && J2<N) ? B[(kk*BLOCK_SIZE+ty)*N + J2] : 0;
+            Bs0[ty][tx+48] = ((kk*BLOCK_SIZE+ty) < N  && J3<N) ? B[(kk*BLOCK_SIZE+ty)*N + J3] : 0;
 
             __syncthreads();
-            #pragma unroll 32  
+            #pragma unroll 16  
             for (unsigned int k=0;k<BLOCK_SIZE;++k) {
                 Cij[0] += As0[ty][k] * Bs0[k][tx];
-                Cij[1] += As0[ty][k] * Bs0[k][tx+32];
-                Cij[2] += As0[ty+32][k] * Bs0[k][tx];
-                Cij[3] += As0[ty+32][k] * Bs0[k][tx+32];
+                Cij[1] += As0[ty][k] * Bs0[k][tx+16];
+                Cij[2] += As0[ty][k] * Bs0[k][tx+32];
+                Cij[3] += As0[ty][k] * Bs0[k][tx+48];
+                Cij[4] += As0[ty+16][k] * Bs0[k][tx];
+                Cij[5] += As0[ty+16][k] * Bs0[k][tx+16];
+                Cij[6] += As0[ty+16][k] * Bs0[k][tx+32];
+                Cij[7] += As0[ty+16][k] * Bs0[k][tx+48];
+                Cij[8] += As0[ty+32][k] * Bs0[k][tx];
+                Cij[9] += As0[ty+32][k] * Bs0[k][tx+16];
+                Cij[10] += As0[ty+32][k] * Bs0[k][tx+32];
+                Cij[11] += As0[ty+32][k] * Bs0[k][tx+48];
+                Cij[12] += As0[ty+48][k] * Bs0[k][tx];
+                Cij[13] += As0[ty+48][k] * Bs0[k][tx+16];
+                Cij[14] += As0[ty+48][k] * Bs0[k][tx+32];
+                Cij[15] += As0[ty+48][k] * Bs0[k][tx+48];
             }
             __syncthreads();
         }
+
         if(I0<N && J0<N)
           C[I0*N+J0] = Cij[0];
         if(I0<N && J1<N)
           C[I0*N+J1] = Cij[1];
+        if(I0<N && J2<N)
+          C[I0*N+J2] = Cij[2];
+        if(I0<N && J3<N)
+          C[I0*N+J3] = Cij[3];
+
         if(I1<N && J0<N)
-          C[I1*N+J0] = Cij[2];
+          C[I1*N+J0] = Cij[4];
         if(I1<N && J1<N)
-          C[I1*N+J1] = Cij[3];
+          C[I1*N+J1] = Cij[5];
+        if(I1<N && J2<N)
+          C[I1*N+J2] = Cij[6];
+        if(I1<N && J3<N)
+          C[I1*N+J3] = Cij[7];
+
+        if(I2<N && J0<N)
+          C[I2*N+J0] = Cij[8];
+        if(I2<N && J1<N)
+          C[I2*N+J1] = Cij[9];
+        if(I2<N && J2<N)
+          C[I2*N+J2] = Cij[10];
+        if(I2<N && J3<N)
+          C[I2*N+J3] = Cij[11];
+
+        if(I3<N && J0<N)
+          C[I3*N+J0] = Cij[12];
+        if(I3<N && J1<N)
+          C[I3*N+J1] = Cij[13];
+        if(I3<N && J2<N)
+          C[I3*N+J2] = Cij[14];
+        if(I3<N && J3<N)
+          C[I3*N+J3] = Cij[15];
     }
 }
